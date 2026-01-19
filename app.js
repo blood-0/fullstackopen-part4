678b4408e2dabe7e149ea9e06521c6c00c04efa0
blood-0 <110762285+blood-0@users.noreply.github.com>
2026-01-19 19:48:11 -0300

    Exercise 4.1 initial structure

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors= require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogs = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to ', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(()=>{
+        logger.info('connected to mongoDB')
+    })
+    .catch((error)=>{
+        logger.error('error connecting to mongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
