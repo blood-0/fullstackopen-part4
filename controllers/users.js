7677647b70fa1fe12fbfbd4618814e3781a80462
blood-0 <110762285+blood-0@users.noreply.github.com>
2026-05-08 14:36:58 -0300

    exercise 4.16:added validations and tests

---
+++ controllers/users.js
@@ -0,0 +1,48 @@
+const usersRouter = require ('express').Router()
+const bcrypt = require ('bcryptjs')
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const {username, name, password} = request.body
+
+    //validaciones basicas
+    if(! username || ! password) {
+        return response.status(400).json({error: 'username and password are required'})
+    }
+
+    if(username.length < 3 ){
+        return response.status(400).json({error: 'username must be at least 3 character long'})
+    }
+
+    if(password.length < 3 ){
+        return response.status(400).json({error: 'password must be at least 3 character long'})
+    }
+    // verificar que el usuario existe
+    const existingUser = await User.findOne({username})
+    if (existingUser) {
+        return response.status(400).json({error: 'username must be unique'})
+    }
+    
+    //encriptar la contrasenia
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    //crear el usuario
+    const user = new User ({
+        username,
+        name,
+        passwordHash
+    })
+    
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+
+})
+
+
+module.exports = usersRouter
---
+++ tests/user_api.test.js
@@ -0,0 +1,116 @@
+const {test,describe, after, beforeEach} = require ('node:test')
+const assert = require ('node:assert')
+const supertest = require ('supertest')
+const mongoose = require ('mongoose')
+const bcrypt = require ('bcryptjs')
+const app = require('../app')
+const User = require('../models/user')
+
+const api = supertest(app)
+
+describe('when there is inittialy one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({username: 'root', name: 'Root User', passwordHash})
+
+        await user.save()
+    })
+
+    test('creation succeds with a fresh username', async () => {
+        const usersAtStart = await User.find({})
+
+        const newUser = {
+            username: 'testuser',
+            name: 'Test User',
+            password: 'password123'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const usersAtEnd = await User.find({})
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1 )
+        const usernames = usersAtEnd.map (u => u.username)
+        assert(usernames.includes(newUser.username))
+
+    })
+
+    test('GET api users returns users', async () => {
+        const response = await api
+            .get('/api/users')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const users = response.body
+        assert(users.length >= 1)
+
+        users.forEach(user => {
+            assert(user.id)
+            assert(!user.passwordHash)
+        })
+    })
+
+    test('creation fails with proper statuscode and message if username is already taken', async () => {
+        const usersAtStart = await User.find({})
+
+        const newUser = {
+            username: 'root',
+            name: 'SuperUser',
+            password: 'password123'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+        
+        const usersAtEnd = await User.find({})
+        assert(result.body.error.includes('username must be unique'))
+        assert.strictEqual(usersAtStart.length, usersAtEnd.length)
+    })
+    test('creation fails if password is too short', async () => {
+        const usersAtStart = await User.find({})
+
+        const newUser = {
+            username: 'validuser',
+            name: 'Valid Name',
+            password: '12'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+
+        const usersAtEnd = await User.find({})
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+
+    test('creation fails if username is too short', async () => {
+        const usersAtStart = await User.find({})
+
+        const newUser = {
+            username: 'AB',
+            name: 'Test Name',
+            password: 'password123'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+
+        const usersAtEnd = await User.find({})
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+
+    after(async () => {
+        await mongoose.connection.close()
+    })
+})
